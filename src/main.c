742536981b0926ed77777e544b2cf7886d595d3e
gar_onn <mail.garonn@gmail.com>
2014-10-14 20:40:38 +0200

    Fixed some fuckups

--- src/main.c
+++ src/main.c
@@ -16,10 +16,13 @@
 #include "genetic_base.h"
 #include "settings.h"
 
+/******* TEST ** INITIATION **************/
 #ifdef TESTINIT
 
-void test() {
+void test()
+{
     polygon poly = {0};
+    polygon poly2 = {0};
     printf("Testing polygon read and contains\n\n");
     assert(polygon_read("../tests/soos.poly", &poly) == 0);
     assert(polygon_contains(1.236, 0.719, &poly) == 0);
@@ -31,18 +34,28 @@ void test() {
     assert(polygon_contains(1.684, 1.681, &poly) == 1);
     assert(polygon_contains(3.000, 1.848, &poly) == 1);
     assert(polygon_contains(2.260, 1.751, &poly) == 1);
-    printf("%s", polygon_contains(4.994639, 0.934278, &poly) ? "T" : "F");
+    assert(polygon_read("../tests/vierkant.poly", &poly2) == 0);
+    assert(polygon_contains(5.000000, -0.0004491, &poly2) == 0);
+    assert(polygon_contains(0.000000, -0.0004491, &poly2) == 0);
+    assert(polygon_contains(5.000000, 15.0004491, &poly2) == 0);
+    assert(polygon_contains(2.500000, 2.5004491, &poly2) == 1);
+
+    //printf("%s", polygon_contains(4.994639, 0.934278, &poly2) ? "T" : "F");
     polygon_print(&poly);
+    
+    polygon_free(&poly);
+    polygon_free(&poly2);
 
 }
 #endif
-
+/*****************************************/
 
 #ifdef TESTRAND
 
 #define TESTRANDNUM 500
 
-void test() {
+void test()
+{
     polygon poly = {0};
     int i;
     point stuff[TESTRANDNUM] = {0};
@@ -51,7 +64,8 @@ void test() {
 
 
     printf("1337\n");
-    for (i = 0; i < TESTRANDNUM; i++) {
+    for (i = 0; i < TESTRANDNUM; i++)
+    {
         printf("%f %f\n", stuff[i].x, stuff[i].y);
 
     }
@@ -62,7 +76,8 @@ void test() {
 
 #ifdef TESPOPULATOR
 
-void test() {
+void test()
+{
     polygon poly = {0};
     population* popu = NULL;
     int best, i;
@@ -77,7 +92,8 @@ void test() {
     best = get_best(popu);
 
     printf("%f\n", popu->list[best].fitness);
-    for (i = 0; i < popu->numpoints; i++) {
+    for (i = 0; i < popu->numpoints; i++)
+    {
         printf("%f %f\n", popu->list[best].points[i].x, popu->list[best].points[i].y);
 
     }
@@ -87,48 +103,89 @@ void test() {
 }
 #endif
 
-int main(int argc, char** argv) {
-    int result=-1;
+int main(int argc, char** argv)
+{
 #if TESTINIT || TESTRAND || TESPOPULATOR
+    /* run test method */
     srand(time(NULL));
     test();
     return 0;
 #else
+    /* normal running */
+    int result = -1;
     polygon poly = {0};
     population* popu = NULL;
     int best, i;
     int numpoints;
-    
-    srand(time(NULL));
 
-    if (argc != 3) {
+    /* Good input */
+    if (argc != 3) /* TODO: chek if number */
+    {
         printf("USAGE: %s num file\n", argv[0]);
         printf("num \t Number of points to add \n"
-                "file \t A file containing data\n\n");
-        polygon_read("../tests/vierkant.poly", &poly);
-        numpoints = 5;
-    } else {
-        polygon_read(argv[2], &poly);
-        numpoints = atoi(argv[1]);
+               "file \t A file containing data\n\n");
+        return -1;
+    }
+
+
+    result = sscanf(argv[1], "%d", &numpoints);
+    if (result == 0)
+    {
+        printf("You must provide a number:\n");
+        printf("You gave '%s'\n", argv[1]);
+        return -1;
+    }
+
+
+    /* Seed the random generator */
+    srand(time(NULL));
+
+    /* reed arguments */
+    numpoints = atoi(argv[1]);
+    result = polygon_read(argv[2], &poly);
+    if (result != 0)
+    {
+        fprintf(stderr, "Could not read/allocate polygon in %s!\n", argv[2]);
+        return -1;
     }
 
 
     result = init_population(NUM_INDIVIDUS, numpoints, &poly, &popu);
-    
-    if(result < 0){
-        fprintf(stderr,"Could not allocate enough space!\n");
+    if (result != 0)
+    {
+        fprintf(stderr, "Could not allocate enough space!\n");
+        return -2;
     }
-    
-    do_iterations(popu, MAX_INTERATIONS);
 
+
+    if (numpoints > 1)
+    {
+        result = do_iterations(popu, MAX_INTERATIONS);
+        if (result < 0)
+        {
+            fprintf(stderr, "Failed iteration (code %d)!\n", result);
+        }
+    }
+    else
+    {
+        /* one point, everything is optimal*/
+        result = 1;
+    }
+    log_dbg("Finished iteration, %d iterations\n", result);
+
+    /*get best*/
     best = get_best(popu);
 
+#ifdef PERFORMANCE_PRINT
+    printf("Iterations:\n%d\nFitness:\n%f\n", result, popu->list[best].fitness);
+#else
+    /* generate output*/
     printf("%f\n", popu->list[best].fitness);
-    for (i = 0; i < popu->numpoints; i++) {
+    for (i = 0; i < popu->numpoints; i++)
+    {
         printf("%f %f\n", popu->list[best].points[i].x, popu->list[best].points[i].y);
-
     }
-
+#endif
     free_population(&popu);
     polygon_free(&poly);
 #endif
--- src/polygon.c
+++ src/polygon.c
@@ -4,16 +4,19 @@
 #include "polygon.h"
 #include "genetic_base.h"
 
-int polygon_init(int size, polygon* poly) {
+int polygon_init(int size, polygon* poly)
+{
 
     int i;
     poly->verts = (vertex*) calloc(size, sizeof (vertex));
-    if (NULL == poly->verts) {
+    if (NULL == poly->verts)
+    {
         return -1;
     }
 
     //Set up links to next en prev
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size; i++)
+    {
         poly->verts[i].next = &(poly->verts[(i + 1) % size]);
         poly->verts[i].prev = &(poly->verts[(size + i - 1) % size]);
     }
@@ -23,7 +26,8 @@ int polygon_init(int size, polygon* poly) {
     return 0;
 }
 
-void polygon_free(polygon* poly){
+void polygon_free(polygon* poly)
+{
     free(poly->verts);
 }
 
@@ -35,11 +39,15 @@ void polygon_free(polygon* poly){
  *         less than 0 on failed malloc or read, 
  * @todo crap
  */
-int polygon_read(char* filename, polygon* poly) {
+int polygon_read(char* filename, polygon* poly)
+{
     FILE *fptr = fopen(filename, "r"); //Open file
     float x, y;
     int num, r, i = 0;
     double dx, dy, a, b;
+
+    if (NULL == fptr)
+        return -1;
 
     //Get number of lines and initiate polygon
     r = fscanf(fptr, "%d", &num);
@@ -49,7 +57,8 @@ int polygon_read(char* filename, polygon* poly) {
     polygon_init(num, poly);
 
 
-    while (EOF != fscanf(fptr, "%f %f\n", &x, &y)) {
+    while (EOF != fscanf(fptr, "%f %f\n", &x, &y))
+    {
         poly->verts[i].id = i;
         poly->verts[i].x = x;
         poly->verts[i].y = y;
@@ -71,7 +80,8 @@ int polygon_read(char* filename, polygon* poly) {
 
     fclose(fptr);
     //Generate a function for each of the edges (y=ax+b)
-    for (i = 0; i < poly->num_vert; i++) {
+    for (i = 0; i < poly->num_vert; i++)
+    {
         dx = (double) poly->verts[i].x - (double) poly->verts[i].next->x;
         dy = (double) poly->verts[i].y - (double) poly->verts[i].next->y;
 
@@ -80,13 +90,13 @@ int polygon_read(char* filename, polygon* poly) {
         poly->verts[i].line_a = a;
         poly->verts[i].line_b = b;
     }
-    
+
     //Rough approximation of diameter
     poly->diagonal = sqrt(
-            (poly->bound_min_x -poly->bound_max_x)*(poly->bound_min_x -poly->bound_max_x) + 
-            (poly->bound_min_y -poly->bound_max_y)*(poly->bound_min_y -poly->bound_max_y)
-            );
-    
+                          (poly->bound_min_x - poly->bound_max_x)*(poly->bound_min_x - poly->bound_max_x) +
+                          (poly->bound_min_y - poly->bound_max_y)*(poly->bound_min_y - poly->bound_max_y)
+                          );
+
     return 0;
 }
 
@@ -94,7 +104,8 @@ int polygon_read(char* filename, polygon* poly) {
  * Prints a representation of the polygon
  * @param poly
  */
-void polygon_print(polygon* poly) {
+void polygon_print(polygon* poly)
+{
     printf("Printing constellation x%px\n", poly);
     printf("The constellation be containd by these boundries\n");
     printf("\tthe brig o' captn Vandewalle at %f North and %f West\n", poly->bound_max_x, poly->bound_max_y);
@@ -102,33 +113,58 @@ void polygon_print(polygon* poly) {
 
 }
 
-int polygon_contains(float x, float y, polygon* poly) {
+int polygon_contains(float x, float y, polygon* poly)
+{
     int i, count = 0;
     double a, b, yintersect;
 
     //Loop over the vertices
-    for (i = 0; i < poly->num_vert; i++) {
+    for (i = 0; i < poly->num_vert; i++)
+    {
         a = poly->verts[i].line_a;
         b = poly->verts[i].line_b;
         yintersect = a * x + b;
 
-        if (!isinf(a)) {
+        if (!isinf(a))
+        {
             //"if not line that is parallel to y axis"
 
             //Use product of differences to determine if between points on line
             double deltaproduct = (poly->verts[i].x - x) * (poly->verts[i].next->x - x);
-            if (deltaproduct <= 0 && yintersect >= y) {
+            if (deltaproduct <= 0 && yintersect >= y)
+            {
 
                 //special case: point on line -> accept
                 if (yintersect == y)
                     return 1;
 
-                if (!(deltaproduct == 0 && poly->verts[i].x == x && poly->verts[i].next->x != x)) {
+                if (!(deltaproduct == 0 && poly->verts[i].x == x && poly->verts[i].next->x != x))
+                {
                     //If deltaproduct == 0 it is exactly beneath a point (don't double count it)
                     count++;
                     if (count > 1)
                         break;
                 }
+            }
+        }
+        else
+        {
+            //line parrallel with y axis
+            if (poly->verts[i].x == x)
+            {
+                if ((poly->verts[i].y - y)*(poly->verts[i].next->y - y) <= 0)
+                {
+                    /* point on line */
+                    return 1;
+                }
+                else
+                {
+                    /* point above or below line*/
+                    if(poly->verts[i].y > y)
+                        count++;
+                }
+                if (count > 1)
+                    break;
             }
         }
     }
@@ -144,17 +180,21 @@ int polygon_contains(float x, float y, polygon* poly) {
  * @param container ALLOCATED pointer to array;
  * @return 0 on succes
  */
-int polygon_random_points(int number, point* container, polygon* poly) {
+int polygon_random_points(int number, point* container, polygon* poly)
+{
     float dx = poly->bound_max_x - poly->bound_min_x;
     float dy = poly->bound_max_y - poly->bound_min_y;
     float x, y;
     int i;
-    for (i = 0; i < number; i++) {
-        do {
+    for (i = 0; i < number; i++)
+    {
+        do
+        {
             x = poly->bound_min_x + ((double) rand() / (double) RAND_MAX) * dx;
             y = poly->bound_min_y + ((double) rand() / (double) RAND_MAX) * dy;
-                    
-        } while (!polygon_contains(x, y, poly));
+
+        }
+        while (!polygon_contains(x, y, poly));
 
         container[i].x = x;
         container[i].y = y;

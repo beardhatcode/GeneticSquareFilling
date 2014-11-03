a009383d96c16950f67536a75f27fc599d4e8bd1
gar_onn <mail.garonn@gmail.com>
2014-11-03 21:35:53 +0100

    Added weighting decrease to extractor

--- tests/param_results/extractorAll.sh
+++ tests/param_results/extractorAll.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for test in {LOVER_PERCENT,MUTATION_DELTA,SELECTION_PRESSURE,MUTATION_1_IN,NUM_INDIVIDUS};
+for test in {LOVER_PERCENT,MUTATION_DELTA,SELECTION_PRESSURE,MUTATION_1_IN,NUM_INDIVIDUS,WEIGHTING_DECREASE};
 do
     for poly in {vierkant,icosagon,soos};
     do
         ../extractor.sh $test $poly,50;
-        ../extractor.sh $test $poly,5;
         ../extractor.sh $test $poly,15;
+        ../extractor.sh $test $poly,5;
     done;
 done;

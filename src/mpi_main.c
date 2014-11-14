fcd41f5c471e4aa1937a918d4629a5fb21273ea9
gar_onn <mail.garonn@gmail.com>
2014-11-14 12:44:24 +0100

    Parrallel it; now with 2 modes (gatherall and circle)

--- src/mpi_main.c
+++ src/mpi_main.c
@@ -41,66 +41,118 @@ void deserialize_individu(float* source, individu* i, int numpoints)
     }
 }
 
-int transfer_individus(population * popu, int task_id)
+int transfer_individus_gather(population * popu, int task_id)
 {
-    int i, j, start_pos, start_pos_pro, procces, target_id;
+    int i, target_id;
     /* sizes */
-    int req_lovers = popu->num_lovers / (NUM_TASKS - 1);
+    int req_lovers = popu->num_lovers;
     int lover_size = (popu->numpoints * 2);
+    int per_target = popu->num_lovers / (NUM_TASKS - 1);
 
     /* Make some space */
-    int selected[req_lovers];
-    float send_indi[req_lovers * lover_size];
-    float recv_indi[NUM_TASKS * req_lovers * lover_size];
-
-    /* Select a->num_lovers/num_pro UNIQUE random individu's */
-    send_indi[0] = 0.0;
+    float send_indi[req_lovers][lover_size];
+    int selected;
+    /* Gather */
+    float recv_indi[NUM_TASKS][per_target][lover_size];
+    int procces;
+    
+    /* Select a->num_lovers random individu's */;
     for (i = 0; i < req_lovers; i++)
     {
-        selected[i] = do_pos_tournament(popu, 2 * popu->size * SELECTION_PRESSURE / 100);
-        for (j = 0; j < i; j++)
-            if (selected[i] == selected[j])
-            {
-                i--;
-                break;
-            }
-        if (popu->list[selected[i]].fitness > send_indi[0])
-        {
-            send_indi[0] = popu->list[selected[i]].fitness;
-        }
-
-    }
-    /* Convert to float-array                       */
-    /* fill array as | pt1.x | pt1.y | pt2.x | ... |*/
-    for (i = 0; i < req_lovers; i++)
-    {
-        start_pos = i * lover_size;
-        serialize_individu(send_indi + start_pos, popu->list + selected[i], popu->numpoints);
+        selected = do_pos_tournament(popu, 2 * popu->size * SELECTION_PRESSURE / 100);
+        serialize_individu(send_indi[i], popu->list + selected, popu->numpoints);
     }
 
-    /* Send data to everyone */
-    MPI_Allgather(send_indi, req_lovers * lover_size, MPI_FLOAT, recv_indi, req_lovers * lover_size, MPI_FLOAT, MPI_COMM_WORLD);
-
-
-    /* Convert back from float-array for each process and add to population */
-    target_id = popu->size; /* first individu to be placed at size:*/
+    MPI_Allgather(send_indi, per_target * lover_size, MPI_FLOAT, recv_indi, per_target * lover_size, MPI_FLOAT, MPI_COMM_WORLD);
+    target_id = popu->size;
     for (procces = 0; procces < NUM_TASKS; procces++)
     {
         if (procces == task_id) continue;
-        start_pos_pro = procces * (req_lovers * lover_size);
-        for (i = 0; i < req_lovers && target_id < popu->size + popu->num_lovers; i++)
+        for (i = 0; i < per_target && target_id < popu->size + popu->num_lovers; i++)
         {
-            /* fill array as | fitness | pt1.x | pt1.y | pt2.x | ... |*/
-            //Define the start of a "block"
-            start_pos = start_pos_pro + i * lover_size;
-
-            deserialize_individu(recv_indi + start_pos, popu->list + target_id, popu->numpoints);
+            /* Define the start of a "block" */
+            deserialize_individu(recv_indi[procces][i], popu->list + target_id, popu->numpoints);
             get_fitness(popu, popu->list + target_id);
+            //printf("\x1b[3%dm %d Received form %d-%d nr %d: %f %f\x1b[0m\n", task_id, task_id, procces, i,target_id, (popu->list + target_id)->fitness, recv_indi[procces][i][0]);
             target_id++;
         }
     }
-
     do_deathmatch(popu, target_id - popu->size);
+
+}
+
+int transfer_individus_island(population * popu, int task_id)
+{
+    int i, target_id;
+
+    /* sizes */
+    int req_lovers = popu->num_lovers;
+    int lover_size = (popu->numpoints * 2);
+    
+    float recv_indi[req_lovers][lover_size];
+    MPI_Request req[req_lovers];
+    MPI_Status status[req_lovers];
+
+    /* Make some space */
+    float send_indi[req_lovers][lover_size];
+    int selected;
+
+    /* Select a->num_lovers random individu's */;
+    for (i = 0; i < req_lovers; i++)
+    {
+        selected = do_pos_tournament(popu, 2 * popu->size * SELECTION_PRESSURE / 100);
+        serialize_individu(send_indi[i], popu->list + selected, popu->numpoints);
+    }
+
+
+    /* Star */
+    //printf("%d is sending 1 to some (%d)\n", task_id, req_lovers);
+    for (i = 0; i < req_lovers; i++)
+    {
+        /* One for peers */
+        target_id = (task_id + 1 + i * (NUM_TASKS / req_lovers)) % NUM_TASKS;
+        MPI_Isend(send_indi[i], lover_size, MPI_FLOAT, target_id, 5, MPI_COMM_WORLD, &(req[i]));
+    }
+    //printf("%d has sent 1 to %d (%d), waiting...\n", task_id, req_lovers, req_lovers);
+    for (i = 0; i < req_lovers; i++)
+    {
+        MPI_Recv(recv_indi[i], lover_size, MPI_FLOAT, MPI_ANY_SOURCE, 5, MPI_COMM_WORLD, &(status[i]));
+    }
+
+    //printf("%d got %d/%d, waiting for recv...\n", task_id, req_lovers, req_lovers);
+
+    target_id = popu->size;
+    for (i = 0; i < req_lovers; i++)
+    {
+        deserialize_individu(recv_indi[i], popu->list + target_id, popu->numpoints);
+        get_fitness(popu, popu->list + target_id);
+        target_id++;
+    }
+
+    for (i = 0; i < req_lovers; i++)
+    {
+        MPI_Wait(&(req[i]), &(status[i]));
+    }
+    //printf("%d done waiting\n", task_id);
+    MPI_Barrier(MPI_COMM_WORLD);
+    do_deathmatch(popu, target_id - popu->size);
+
+}
+
+int transfer_individus(population * popu, int task_id)
+{
+
+
+    if (popu->num_lovers / (NUM_TASKS - 1) >= 1)
+    {
+        transfer_individus_gather(popu, task_id);
+    }
+    else
+    {
+        transfer_individus_island(popu, task_id);
+    }
+    /* Convert back from float-array for each process and add to population */
+
     return 0;
 }
 
@@ -146,17 +198,19 @@ int main(int argc, char *argv[])
         return r;
     }
 
+    return 0;
 }
 
 int mpi_parse_input(int argc, char *argv[], polygon* poly, int* num)
 {
+    /* handle errors */
     int r = polygon_read(argv[2], poly);
     *num = atoi(argv[1]);
     return r;
 }
 
 int parrallel_loops(polygon* poly, int numpoints, int id, int pop_size, int itters, loop_checker end_loop)
-{ // <editor-fold defaultstate="collapsed">
+{ /* <editor-fold defaultstate="collapsed"> */
     int r, i;
     int loopsatatus;
     individu* best_ndi;
@@ -206,13 +260,15 @@ int parrallel_loops(polygon* poly, int numpoints, int id, int pop_size, int itte
         {
             /* Put best in front of our list...*/
             best_ndi = popu->list;
-            {
-                float recv_indi[popu->numpoints * 2];
-                MPI_Status status;
-                MPI_Recv(recv_indi, popu->numpoints * 2, MPI_FLOAT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-                deserialize_individu(recv_indi, popu->list, popu->numpoints);
-                get_fitness(popu, popu->list);
-            }
+
+            float recv_indi[popu->numpoints * 2];
+            MPI_Status status;
+
+            MPI_Recv(recv_indi, popu->numpoints * 2, MPI_FLOAT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            deserialize_individu(recv_indi, popu->list, popu->numpoints);
+
+            get_fitness(popu, popu->list);
+
         }
         printf("%.9f\n", best_ndi->fitness);
         for (i = 0; i < popu->numpoints; i++)
@@ -236,7 +292,7 @@ int parrallel_loops(polygon* poly, int numpoints, int id, int pop_size, int itte
     }
     free_population(&popu);
     return 0;
-}// </editor-fold>	
+}/* </editor-fold> */
 
 int master_loop(population* a, int done_itters, int itters, int id)
 {
@@ -245,7 +301,7 @@ int master_loop(population* a, int done_itters, int itters, int id)
     double gathered_best[NUM_TASKS * 2];
     static int total_itters = 0;
     int i;
-    int time_to_end = 1;
+    int loops_saturated = 1;
     static int times_to_end = 0;
     static int times_to_end_2 = 0;
     double cur_best;
@@ -265,8 +321,8 @@ int master_loop(population* a, int done_itters, int itters, int id)
     {
         if (gathered_best[2 * i + 1] > 0.95)
         {
-            printf("No convergence yet %d did %f of loops\n", i, gathered_best[2 * i + 1]);
-            time_to_end = 0; //One of the things did not converge
+            //printf("No convergence yet %d did %f of loops\n", i, gathered_best[2 * i + 1]);
+            loops_saturated = 0; /*One of the things did not converge*/
         }
         if (gathered_best[2 * i] > cur_best)
         {
@@ -280,12 +336,12 @@ int master_loop(population* a, int done_itters, int itters, int id)
     {
         printf("Can't ignore change %f\n", fabs(prev_best - cur_best));
         prev_best = cur_best;
-        time_to_end = 0;
+        loops_saturated = 0;
         times_to_end_2 = 0;
     }
 
 
-    if (time_to_end || times_to_end_2 > 10)
+    if (loops_saturated || times_to_end_2 > 10)
     {
         printf("\x1b[31mIt is time (%d)\x1b[0m\n", times_to_end);
         times_to_end++;
@@ -299,23 +355,39 @@ int master_loop(population* a, int done_itters, int itters, int id)
 
     if (times_to_end > 3)
     {
-        /* Send end signal */
-        for (i = 1; i < NUM_TASKS; i++)
-            MPI_Send(&best_pro, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        /* Send */
+        {
+            MPI_Request endrequests[NUM_TASKS - 1];
+            /* Send end signal */
+            for (i = 1; i < NUM_TASKS; i++)
+                MPI_Isend(&best_pro, 1, MPI_INT, i, 55, MPI_COMM_WORLD, &(endrequests[i - 1]));
+
+            MPI_Status status;
+            for (i = 1; i < NUM_TASKS; i++)
+                MPI_Wait(&(endrequests[i - 1]), &status);
+
+            /* transfer individus */
+            if (NUM_TASKS > 1)
+                transfer_individus(a, id);
+
+
+            return best_pro == id ? GENETIC_BEST : GENETIC_NOT_BEST;
+        }
+
     }
-
-
-    /* transfer individus */
-    if (NUM_TASKS > 1)
-        transfer_individus(a, id);
-
-
-    if (times_to_end > 3)
+    else
     {
-        return best_pro == id ? GENETIC_BEST : GENETIC_NOT_BEST;
+
+        /* transfer individus */
+        if (NUM_TASKS > 1)
+            transfer_individus(a, id);
     }
 
-    return GENETIC_CONTINUE; //1;//done_itters<itters ? 0 : 1;
+
+
+
+
+    return GENETIC_CONTINUE;
 }
 
 int slave_loop(population* a, int done_itters, int itters, int id)
@@ -330,35 +402,32 @@ int slave_loop(population* a, int done_itters, int itters, int id)
     /* send the best data to MASTER */
     send_stat[0] = a->best;
     send_stat[1] = (double) done_itters / (double) itters;
-
     MPI_Gather(send_stat, 2, MPI_DOUBLE, NULL, 2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    //A end broadcast might occur here (in master_loop)
+    /*A end broadcast might occur here (in master_loop) */
 
     /* Transfer individus */
     transfer_individus(a, id);
-
-    MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
+    MPI_Iprobe(MPI_ANY_SOURCE, 55, MPI_COMM_WORLD, &flag, &status);
 
     if (flag)
     {
-        MPI_Recv(&flag, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        //printf("%d got end msg   best %d!!!!!!!!!\n", id, flag);
+        MPI_Recv(&flag, 1, MPI_INT, MPI_ANY_SOURCE, 55, MPI_COMM_WORLD, &status);
         return flag == id ? GENETIC_BEST : GENETIC_NOT_BEST;
     }
 
-    return GENETIC_CONTINUE; //done_itters<itters ? 0 : 1;
+    return GENETIC_CONTINUE;
 }
 
 int master_main(polygon* poly, int numpoints, int id)
 {
-    parrallel_loops(poly, numpoints, id, NUM_INDIVIDUS / 2, 100, &master_loop);
+    parrallel_loops(poly, numpoints, id, NUM_INDIVIDUS / 2, 50, &master_loop);
     return 0;
 }
 
 int slave_main(polygon* poly, int numpoints, int id)
 {
-    parrallel_loops(poly, numpoints, id, NUM_INDIVIDUS / 2, 100, &slave_loop);
+    parrallel_loops(poly, numpoints, id, NUM_INDIVIDUS / 2, 50, &slave_loop);
 
     return 0;
 }
